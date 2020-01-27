019fefaca19a246e7886c08c7fb0470eb7a99297
Christian Alfoni <christianalfoni@gmail.com>
2020-01-27 13:44:39 +0100

    initial design system

--- src/config/transform-css.ts
+++ src/config/transform-css.ts
@@ -1,6 +1,12 @@
+import * as CSS from 'csstype';
+
 import { IConfig } from '../types';
 
-const classes = {
+type TCssConfig = Omit<IConfig, 'aliases' | 'breakpoints'>;
+
+const classes: {
+  [key in keyof TCssConfig]: Array<keyof CSS.StandardShorthandProperties | keyof CSS.StandardPropertiesHyphen>;
+} = {
   space: [
     'margin',
     'margin-top',
@@ -12,10 +18,27 @@ const classes = {
     'padding-right',
     'padding-bottom',
     'padding-left',
-    'grid-gap',
-    'grid-column-gap',
-    'grid-row-gap',
   ],
+  colors: ['color', 'background-color'],
+  sizes: ['width', 'height', 'min-width', 'max-width', 'min-height', 'max-height'],
+  borderStyles: ['border-style', 'border-bottom-style', 'border-top-style', 'border-left-style', 'border-right-style'],
+  borderWidths: ['border-width', 'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width'],
+  borderColors: ['border-color', 'border-bottom-color', 'border-top-color', 'border-left-color', 'border-right-color'],
+  radii: [
+    'border-radius',
+    'border-top-left-radius',
+    'border-top-right-radius',
+    'border-bottom-right-radius',
+    'border-bottom-left-radius',
+  ],
+  fontSizes: ['font-size'],
+  fontWeights: ['font-weight'],
+  fonts: ['font-family'],
+  letterSpacings: ['letter-spacing'],
+  lineHeights: ['line-height'],
+  shadows: ['box-shadow', 'text-shadow'],
+  transitions: ['transition'],
+  zIndices: ['z-index'],
 };
 
 export interface ICssClasses {
@@ -38,9 +61,9 @@ function buildClassesFromObject(cssProperty: string, value: { [key: string]: str
   }, {} as ICssClasses);
 }
 
-function getSpaceClasses(config: IConfig) {
-  return classes.space.reduce((allAggr, cssProperty) => {
-    const value = typeof config.space === 'function' ? config.space(config) : config.space;
+function getClasses(key: keyof TCssConfig, config: IConfig) {
+  return classes[key].reduce((allAggr, cssProperty) => {
+    const value = typeof config[key] === 'function' ? (config[key] as any)(config) : config[key];
 
     if (Array.isArray(value)) {
       return {
@@ -59,7 +82,21 @@ function getSpaceClasses(config: IConfig) {
 export const transform = (config: IConfig) => {
   const cssClasses: ICssClasses = {};
 
-  Object.assign(cssClasses, getSpaceClasses(config));
+  Object.assign(cssClasses, getClasses('space', config));
+  Object.assign(cssClasses, getClasses('colors', config));
+  Object.assign(cssClasses, getClasses('sizes', config));
+  Object.assign(cssClasses, getClasses('borderStyles', config));
+  Object.assign(cssClasses, getClasses('borderWidths', config));
+  Object.assign(cssClasses, getClasses('borderColors', config));
+  Object.assign(cssClasses, getClasses('radii', config));
+  Object.assign(cssClasses, getClasses('fontSizes', config));
+  Object.assign(cssClasses, getClasses('fontWeights', config));
+  Object.assign(cssClasses, getClasses('fonts', config));
+  Object.assign(cssClasses, getClasses('letterSpacings', config));
+  Object.assign(cssClasses, getClasses('lineHeights', config));
+  Object.assign(cssClasses, getClasses('shadows', config));
+  Object.assign(cssClasses, getClasses('transitions', config));
+  Object.assign(cssClasses, getClasses('zIndices', config));
 
   return cssClasses;
 };
--- src/types.ts
+++ src/types.ts
@@ -8,28 +8,25 @@ export type TClassyUi<T extends TClasses = TClasses> = (...args: Array<TArgs<T>>
 
 export type TPseudoClass<T extends TClasses = TClasses> = (className: T) => TClassyUiString;
 
-export type IConfigValue<T> = ((config: IConfig) => T) | T;
+export type IConfigValue<T> = ((config: IConfig) => IConfigValue<T>) | T;
 
 export interface IConfig {
   space: IConfigValue<string[] | { [key: string]: string }>;
   fontSizes: IConfigValue<string[]>;
-  breakpoints: IConfigValue<string[]>;
+  breakpoints: IConfigValue<{ [name: string]: string }>;
   colors: IConfigValue<{ [name: string]: string | string[] | { [name: string]: string } }>;
   fonts: IConfigValue<{ [name: string]: string }>;
   fontWeights: IConfigValue<string[] | { [name: string]: string }>;
   lineHeights: IConfigValue<string[] | { [name: string]: string }>;
   letterSpacings: IConfigValue<string[] | { [name: string]: string }>;
   sizes: IConfigValue<string[] | { [name: string]: string }>;
-  borders: IConfigValue<{ [name: string]: string }>;
+  borderColors: IConfigValue<{ [name: string]: string | string[] | { [name: string]: string } }>;
   borderWidths: IConfigValue<string[] | { [name: string]: string }>;
   borderStyles: IConfigValue<{ [name: string]: string }>;
   radii: IConfigValue<string[] | { [name: string]: string }>;
   shadows: IConfigValue<string[] | { [name: string]: string }>;
   zIndices: IConfigValue<number[] | { [name: string]: number }>;
   transitions: IConfigValue<{ [name: string]: string }>;
-  mediaQueries: IConfigValue<{
-    [name: string]: string;
-  }>;
   aliases?: Partial<{
     fontSizes: IConfigValue<void>;
     space: IConfigValue<void>;
