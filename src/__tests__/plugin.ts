42bb98295271565df4ec351adf7476fc90810fde
Christian Alfoni <christianalfoni@gmail.com>
2020-02-23 22:15:20 +0100

    fix tests

--- src/__tests__/plugin.ts
+++ src/__tests__/plugin.ts
@@ -8,7 +8,6 @@ pluginTester({
   snapshot: true,
   tests: [
     {
-      only: true,
       title: 'should convert simple',
       code: `
 import { compose, tokens } from 'classy-ui'
@@ -18,110 +17,89 @@ compose(tokens.color.red)`,
     {
       title: 'should convert pseudos',
       code: `
-import { c, hover } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-c(hover('color-red'))`,
+compose(tokens.color.red.hover)`,
     },
     {
       title: 'should convert breakpoints',
       code: `
-import { c, md } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-c(md('color-red'))`,
+compose(tokens.color.red.md)`,
     },
     {
       title: 'should convert both breakpoints and pseudos',
       code: `
-import { c, hover, md } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-c(md(hover('color-red')))`,
+compose(tokens.color.red.md.hover)`,
     },
     {
       title: 'should convert complex compositions',
       code: `
-import { c, hover, md, lg } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-c('color-blue', md(lg(hover('color-red'))))`,
+compose(tokens.color.blue, tokens.color.red.md.lg.hover)
+`,
     },
     {
       error: true,
       title: 'should throw when nesting c call',
       code: `
-import { c } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-c(c('color-blue'))`,
+compose(compose(tokens.color.blue))`,
     },
     {
       error: false,
       title: 'should not throw when using group inside c call',
       code: `
-import { c, group } from 'classy-ui'
+import { compose, group } from 'classy-ui'
 
-c(group())`,
-    },
-    {
-      error: false,
-      title: 'should allow arguments to group',
-      code: `
-import { group } from 'classy-ui'
-
-group('color-blue', 'color-red')`,
-    },
-    {
-      title: 'should give group a classname',
-      code: `
-import { c, group } from 'classy-ui'
-
-group()`,
-    },
-    {
-      title: 'should allow pseudo decorators as base',
-      code: `
-import { c, hover } from 'classy-ui'
-
-hover('color-red')`,
+compose(group)`,
     },
     {
       title: 'should allow group with conditional after it',
       code: `
-import { c, group } from 'classy-ui'
+import { compose, group, tokens } from 'classy-ui'
 
-c(group(), { 'color-red': true })`,
+compose(group, true ? tokens.color.red : '')`,
     },
     {
       title: 'should handle complex dynamic composition',
       code: `
-import { c, group } from 'classy-ui'
+import { compose, group, tokens } from 'classy-ui'
 
-c(group(), { 'color-red': true }, 'background-color-red', { 'display-block': true }, 'display-inline-block')`,
+c(group, true ? tokens.color.red : '', tokens.backgroundColor.red, true ? tokens.display.block : '', tokens.display.inline)`,
     },
     {
       title: 'should fix specifiy issue in dynamic composition',
       code: `
-import { c} from 'classy-ui'
-const compose = c('color-blue', 'background-color-blue')
-const moreCompose = c('color-green', 'background-color-green')
+import { compose, tokens } from 'classy-ui'
+const aCompose = compose(tokens.color.blue, tokens.backgroundColor.blue)
+const moreCompose = compose(tokens.color.green, tokens.backgroundColor.green)
 
-c(compose, moreCompose, 'color-red', 'background-color-red')`,
+compose(aCompose, moreCompose, tokens.color.red, tokens.backgroundColor.red)`,
     },
     {
       title: 'should handle dynamic calls to some function',
       code: `
-import { c, group } from 'classy-ui'
+import { compose, group } from 'classy-ui'
 
-c(group(), someFunction())`,
+compose(group, someFunction())`,
     },
     {
       title: 'should fix specificity inside object',
       code: `
-import { c, hover } from 'classy-ui'
+import { compose, tokens } from 'classy-ui'
 
-const buttonBlue = c('color-blue');
-const buttonGreen = c('color-green');
+const buttonBlue = compose(tokens.color.blue);
+const buttonGreen = compose(tokens.color.green);
 
-const buttonRed = c(buttonBlue, buttonGreen, 'color-red');
+const buttonRed = compose(buttonBlue, buttonGreen, tokens.color.red);
 
-{className: c(buttonRed, hover('color-green'))}
+{className: compose(buttonRed, tokens.color.green.hover)}
 
 
 `,
