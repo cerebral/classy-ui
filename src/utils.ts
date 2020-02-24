072b15c276bf32bdc41ad756f1611415470c03e8
Christian Alfoni <christianalfoni@gmail.com>
2020-02-24 19:52:07 +0100

    change delimiter to @

--- src/utils.ts
+++ src/utils.ts
@@ -307,7 +307,7 @@ export const injectDevelopment = (classCollection: IExtractedClasses, classes: I
     return aggr.concat(
       classnameKeys.reduce((injections, classnameKey) => {
         const classConfig = config.classnames[classnameKey];
-        const name = `${camelToDash(classnameKey)}-${configClass.token}`;
+        const name = `${camelToDash(classnameKey)}\\\@${configClass.token}`;
         const classname = prefix + name;
         const classEntry = createClassEntry(classname, otherDecorators, evaluatedName =>
           (classConfig.css as any)(evaluatedName, classConfig.tokens[configClass.token]),
@@ -382,7 +382,7 @@ export const createClassObject = (
   isProduction: boolean,
 ): IExtractedClass => {
   const id = `${camelToDash(baseClass)}-${token}`;
-  const uid = [decorators.sort().join(':'), id]
+  const uid = [decorators.sort().join(':'), `${camelToDash(baseClass)}@${token}`]
     .filter(Boolean)
     .filter(i => i!.length > 0)
     .join(':');
@@ -393,7 +393,7 @@ export const createClassObject = (
   if (id && isProduction && classes[id].derived) {
     name = classes[id]
       .derived!.reduce((aggr, key) => {
-        return aggr.concat(classes[id].shortName);
+        return aggr.concat(classes[`${camelToDash(key)}-${token}`].shortName);
       }, [] as string[])
       .join(' ');
   } else if (id && isProduction) {
@@ -401,7 +401,7 @@ export const createClassObject = (
   } else if (id && classes[id].derived) {
     name = classes[id]
       .derived!.reduce((aggr, key) => {
-        return aggr.concat(`${camelToDash(key)}-${token}`);
+        return aggr.concat(`${camelToDash(key)}@${token}`);
       }, [] as string[])
       .join(' ');
   } else {
