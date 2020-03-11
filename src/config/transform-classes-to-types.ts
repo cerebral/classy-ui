f63bdfd01a3b43e3f6ffd77a6ebd0f578120c6c0
Christian Alfoni <christianalfoni@gmail.com>
2020-03-11 16:47:58 +0100

    fix property name and add readonly

--- src/config/transform-classes-to-types.ts
+++ src/config/transform-classes-to-types.ts
@@ -40,125 +40,125 @@ function convertClassnameToType(baseClass: string, token: string, config: IEvalu
    * ${css.join('\n')}
    * \`\`\`
    */
-  "${token}": IDecorators;
+  readonly ${token}: IDecorators;
   `;
 }
 
 export const transform = (config: IEvaluatedConfig) => {
   return `
 declare module 'classy-ui' {
-export type TClassname = string & "CLASSNAME";
-export enum Themes {
-  ${config.themeNames
-    .reduce<string[]>((aggr, theme) => aggr.concat(`${theme} = "${`themes-${theme}"`}`), [])
-    .join(',\n')}
-}
-export type TGroup = 'GROUP'
-export interface IDecorators {
-  /**
-   * \`\`\`css
-   * group:hover $token {}
-   * \`\`\`
-   */
-  groupHover: IDecorators;
-  /**
-   * \`\`\`css
-   * group:focus $token {}
-   * \`\`\`
-   */
-  groupFocus: IDecorators;
-  /**
-   * \`\`\`css
-   * group:active $token {}
-   * \`\`\`
-   */
-  groupActive: IDecorators;
-  /**
-   * \`\`\`css
-   * group:first-child $token {}
-   * \`\`\`
-   */
-  groupFirstChild: IDecorators;
-  /**
-   * \`\`\`css
-   * group:last-child $token {}
-   * \`\`\`
-   */
-  groupLastChild: IDecorators;
-  /**
-   * \`\`\`css
-   * group:odd-child $token {}
-   * \`\`\`
-   */
-  groupOddChild: IDecorators;
-  /**
-   * \`\`\`css
-   * group:even-child $token {}
-   * \`\`\`
-   */
-  groupEvenChild: IDecorators;
-  /**
-   * \`\`\`css
-   * group:focus-within $token {}
-   * \`\`\`
-   */
-  groupFocusWithin: IDecorators;
-${allowedPseudoDecorators
-  .map(
-    decorator => `
-  /**
-   * \`\`\`css
-   * $token:${camelToDash(decorator)} {}
-   * \`\`\`
-   */
-  ${decorator}: IDecorators;
-`,
-  )
-  .join('\n')}
-}
-export type TCompose = (...args: Array<IDecorators | TGroup | Themes | TClassname | boolean | ''>) => TClassname;
-export type TTokens = { 
-  ${Object.keys(config.classnames)
-    .reduce<string[]>((aggr, baseClass) => {
-      return aggr.concat(`
-  ${
-    config.classnames[baseClass].description
-      ? `
-/**
-  * ${config.classnames[baseClass].description}
-  */`
-      : ''
+  export type TClassname = string & "CLASSNAME";
+  export enum Themes {
+    ${config.themeNames
+      .reduce<string[]>((aggr, theme) => aggr.concat(`${theme} = "${`themes-${theme}"`}`), [])
+      .join(',\n')}
   }
-  ${baseClass}: {
-    ${Object.keys(config.classnames[baseClass].tokens)
-      .reduce<string[]>((subAggr, token) => {
-        return subAggr.concat(convertClassnameToType(baseClass, token, config));
+  export type TGroup = 'GROUP'
+  export interface IDecorators {
+    /**
+     * \`\`\`css
+     * group:hover $token {}
+     * \`\`\`
+     */
+    groupHover: IDecorators;
+    /**
+     * \`\`\`css
+     * group:focus $token {}
+     * \`\`\`
+     */
+    groupFocus: IDecorators;
+    /**
+     * \`\`\`css
+     * group:active $token {}
+     * \`\`\`
+     */
+    groupActive: IDecorators;
+    /**
+     * \`\`\`css
+     * group:first-child $token {}
+     * \`\`\`
+     */
+    groupFirstChild: IDecorators;
+    /**
+     * \`\`\`css
+     * group:last-child $token {}
+     * \`\`\`
+     */
+    groupLastChild: IDecorators;
+    /**
+     * \`\`\`css
+     * group:odd-child $token {}
+     * \`\`\`
+     */
+    groupOddChild: IDecorators;
+    /**
+     * \`\`\`css
+     * group:even-child $token {}
+     * \`\`\`
+     */
+    groupEvenChild: IDecorators;
+    /**
+     * \`\`\`css
+     * group:focus-within $token {}
+     * \`\`\`
+     */
+    groupFocusWithin: IDecorators;
+  ${allowedPseudoDecorators
+    .map(
+      decorator => `
+    /**
+     * \`\`\`css
+     * $token:${camelToDash(decorator)} {}
+     * \`\`\`
+     */
+    ${decorator}: IDecorators;
+  `,
+    )
+    .join('\n')}
+  }
+  export type TCompose = (...args: Array<IDecorators | TGroup | Themes | TClassname | boolean | ''>) => TClassname;
+  export type TTokens = { 
+    ${Object.keys(config.classnames)
+      .reduce<string[]>((aggr, baseClass) => {
+        return aggr.concat(`
+    ${
+      config.classnames[baseClass].description
+        ? `
+  /**
+    * ${config.classnames[baseClass].description}
+    */`
+        : ''
+    }
+    ${baseClass}: {
+      ${Object.keys(config.classnames[baseClass].tokens)
+        .reduce<string[]>((subAggr, token) => {
+          return subAggr.concat(convertClassnameToType(baseClass, token, config));
+        }, [])
+        .join('\n')}
+    }
+  `);
       }, [])
       .join('\n')}
   }
-`);
-    }, [])
+
+  export const themes: {
+    ${config.themeNames.reduce<string[]>((aggr, theme) => aggr.concat(`${theme}: Themes.${theme}`), []).join('\n')}
+  };
+  export const group: TGroup;
+  export const compose: TCompose;
+  export const tokens: TTokens;
+
+  ${Object.keys(config.screens)
+    .map(
+      screen => `
+    /**
+     * \`\`\`css
+     * \\${config.screens[screen](`\n      $token\n`, getScreens(config))}
+     * \`\`\`
+     */
+    export const ${screen}: TCompose;
+  `,
+    )
     .join('\n')}
-}
-
-export const themes: {
-  ${config.themeNames.reduce<string[]>((aggr, theme) => aggr.concat(`${theme}: Themes.${theme}`), []).join('\n')}
-};
-export const group: TGroup;
-export const compose: TCompose;
-export const tokens: TTokens;
-
-${Object.keys(config.screens)
-  .map(
-    screen => `
-  /**
-   * \`\`\`css
-   * \\${config.screens[screen](`\n      $token\n`, getScreens(config))}
-   * \`\`\`
-   */
-  export const ${screen}: TCompose;
-`,
-  )
-  .join('\n')}
 }`;
 };
